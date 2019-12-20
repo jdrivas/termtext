404f9b8cfe7bd704e76ea773a642bbe0932e965e
David Rivas <jdr@rivas-yee.com>
2019-12-20 10:06:35 -0800

    pulled viper config out

---
+++ config.go
@@ -0,0 +1,21 @@
+
+// JSONDisplayKey controls output.
+// If set only JSON is displayed
+// for renders which have response objects.
+// For example, in a yaml configuration file you can set:
+// Termtext:
+//    JSONDisplay: True
+//
+const JSONDisplayKey = "Termtext.JSONDisplay" // bool
+
+// This is how you pick a screen profile.
+
+// Variables
+const (
+	ScreenProfileKey        = "Termtext.screenProfile" // Viper Key for profile name - string
+)
+
+// Values
+ScreenNoColorDefaultKey = "termtextNoColor"        // No Color profile
+ScreenDarkDefaultKey    = "termtextDarkDefault"    // Dark Profile
+ScreenLightDefaultKey   = "termtextLightDefault"   // Light Profile
